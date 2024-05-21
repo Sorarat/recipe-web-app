796dacb043ce72effe8e64c9cafb4d63cd2e73f2
Sorarat Urairaksa <sorarat.urairaksa@gmail.com>
2024-05-21 10:03:21 +0800

    updated hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='w-full mt-[80px] h-[500px] bg-gray-200 flex flex-col items-center justify-center'>
-        <div>
+    <div className='w-full mt-[80px] h-[500px] bg-gray-200 flex flex-row items-center justify-between px-10'>
+
+        {/* left side */}
+        <div className='flex flex-col'>
             <h3 className='text-3xl'>Title</h3>
+            
         </div>
         
         <div>
@@ -12,8 +15,10 @@ const Hero = () => {
             <p>Nutrients Info</p>
         </div> 
 
-        {/* pics */}
-       
+        {/* right side: pics */}
+        <div className='w-1/3 h-full bg-gray-400 flex items-center justify-center'>
+            <div className='w-40 h-40 border border-gray-600'></div>
+        </div>
 
     </div>
 
